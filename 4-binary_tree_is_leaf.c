4af86e7fda7c0388da491ec9c8d5cbbcb317e834
kaytee07 <kofi.taylor07@gmail.com>
2023-05-31 14:46:12 +0000

    check if  node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if a node has no children
+ * @node: this is a pointer to the node to check
+ * Return: 1 if node is a leaf and 0 if it isn't
+ */
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL)
+return (0);
+
+if (node->left || node->right)
+return (0);
+else
+return (1);
+}
