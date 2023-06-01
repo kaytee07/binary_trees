118ff2aa826d51717114562cad56ed530c3bf296
kaytee07 <kofi.taylor07@gmail.com>
2023-06-01 17:34:47 +0000

    correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,15 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 if (tree == NULL)
-return (-1);
+return (0);
 
 size_t left_height = binary_tree_height(tree->left);
 size_t right_height = binary_tree_height(tree->right);
 
-return (left_height > right_height ? left_height + 1 : right_height + 1);
+if (left_height > right_height)
+return left_height + 1;
+else
+return right_height + 1;
+
+return (0);
 }
