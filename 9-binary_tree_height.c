58e0a7f6450ef7049f6779144a2be57f710dc165
kaytee07 <kofi.taylor07@gmail.com>
2023-06-01 17:22:28 +0000

    find height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,35 +2,6 @@
 #include <stdio.h>
 
 /**
- * check_height - this function find he max depth
- * @tree: this is the pointer to root of node
- * @n: this is the current height of tree
- * Return: return the height of the binary tree
- */
-
-size_t check_height(const binary_tree_t *tree, int n)
-{
-if (tree->left || tree->right)
-{
-if (tree->left && tree->right)
-{
-check_height(tree->left, n++);
-check_height(tree->right, n++);
-}
-else if (tree->left)
-{
-check_height(tree->left, n++);
-}
-else if (tree->right)
-{
-printf("%d:%d->\n", tree->n, n);
-check_height(tree->right, n++);
-}
-}
-return (n);
-}
-
-/**
  * binary_tree_height - this find the height of the binary tree
  * @tree: root of the node to which height is measured
  * Return: return the height if successful or 0 if otherwise
@@ -39,6 +10,10 @@ return (n);
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 if (tree == NULL)
-return (0);
-return (check_height(tree, 0));
+return (-1);
+
+size_t left_height = binary_tree_height(tree->left);
+size_t right_height = binary_tree_height(tree->right);
+
+return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
