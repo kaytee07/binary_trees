b3c92ef9c9653d8b791de9c119d18238cb09ec61
kaytee07 <kofi.taylor07@gmail.com>
2023-06-01 19:31:53 +0000

    add

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,7 +25,7 @@ return (0);
  * Return: 1 if its a full binary tree or 0 if not
  */
 
-int binary_tree_full(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
@@ -34,7 +34,7 @@ if (tree->left == NULL && tree->right == NULL)
 return (1);
 
 if (tree->left != NULL && tree->right != NULL)
-return (binary_tree_full(tree->left) * binary_tree_full(tree->right));
+return (binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right));
 
 return (0);
 }
