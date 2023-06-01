293803120a27d5910eb02b41b367f43ca496f0c8
kaytee07 <kofi.taylor07@gmail.com>
2023-06-01 16:45:41 +0000

    identify the unclude of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_uncle - finds the uncle of a node in a binary tree
+ * @node: pointer to the node to find the uncle
+ * Return: pointer to the uncle node, or NULL if no uncle or node is NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+return NULL;
+
+binary_tree_t *parent = node->parent;
+binary_tree_t *grandparent = parent->parent;
+
+if (grandparent->left == parent)
+return grandparent->right;
+else
+return grandparent->left;
+}
