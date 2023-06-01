70906f712c475fd751448b2d55f73a4a1a8d9af7
kaytee07 <kofi.taylor07@gmail.com>
2023-06-01 19:08:14 +0000

    correction

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-return NULL;
+return (NULL);
 
 binary_tree_t *parent = node->parent;
 binary_tree_t *grandparent = parent->parent;
