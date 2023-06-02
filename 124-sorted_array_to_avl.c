af19514f1316bf24044ff677ffbb1c4dcf16bf75
kaytee07 <kofi.taylor07@gmail.com>
2023-06-02 12:56:04 +0000

    sort array

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,28 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * _sorted_array_to_avl - helper function for sorted_array_to_avl
- * @array: inputs arr
- * @start: starts index
- * @end: end index
+ * _sorted_array_to_avl - helper func for sorted_array_to_avl
+ * @array: input array
+ * @start: starting index
+ * @end: ending index
  * @parent: pointer to parent node
  * Return: newly created node
  */
 avl_t *_sorted_array_to_avl(int *array, int start, int end, avl_t *parent)
 {
-avl_t *new;
-int mid;
-if (start > end)
-return (NULL);
-mid = (start + end) / 2;
-new = calloc(1, sizeof(avl_t));
-if (!new)
-return (NULL);
-new->n = array[mid];
-new->parent = parent;
-new->left = _sorted_array_to_avl(array, start, mid - 1, new);
-new->right = _sorted_array_to_avl(array, mid + 1, end, new);
-return (new);
+	avl_t *new;
+	int mid;
+
+	if (start > end)
+		return (NULL);
+	mid = (start + end) / 2;
+	new = calloc(1, sizeof(avl_t));
+	if (!new)
+		return (NULL);
+	new->n = array[mid];
+	new->parent = parent;
+	new->left = _sorted_array_to_avl(array, start, mid - 1, new);
+	new->right = _sorted_array_to_avl(array, mid + 1, end, new);
+	return (new);
 }
 
 
@@ -34,7 +35,8 @@ return (new);
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-if (!array)
-return (NULL);
-return (_sorted_array_to_avl(array, 0, size - 1, NULL));
+
+	if (!array)
+		return (NULL);
+	return (_sorted_array_to_avl(array, 0, size - 1, NULL));
 }
