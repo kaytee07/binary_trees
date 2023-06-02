0840cc36b929d75f6b51ae52241118f872e427d7
kaytee07 <kofi.taylor07@gmail.com>
2023-06-02 12:11:32 +0000

    insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,50 +1,48 @@
-#include <stddef.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
-
 /**
- * bst_insert - insert into the binary_search_tree
- * @tree: bst to insert the new node
- * @value: value to be inserted in the new node
- * Return: new node or NULL
+ * bst_insert - Inserts a value in a Binary Search Tree.
+ * @tree: A double pointer to the root node of the BST to insert the value.
+ * @value: The value to store in the node to be inserted.
+ *
+ * Return: A pointer to the created node, or NULL on failure.
  */
-binary_tree_t *bst_insert(binary_tree_t **tree, int value)
+bst_t *bst_insert(bst_t **tree, int value)
 {
-if (tree == NULL)
-return (NULL);
+	bst_t *curr, *new;
 
-if (*tree == NULL)
-{
-*tree = binary_tree_node(NULL, value);
-return (*tree);
-}
+	if (tree != NULL)
+	{
+		curr = *tree;
 
-binary_tree_t *current = *tree;
+		if (curr == NULL)
+		{
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
 
-while (1)
-{
-if (value < current->n)
-{
-if (current->left == NULL)
-{
-current->left = binary_tree_node(current, value);
-return (current->left);
-}
-current = current->left;
-}
-else if (value > current->n)
-{
-if (current->right == NULL)
-{
-current->right = binary_tree_node(current, value);
-return (current->right);
-}
-current = current->right;
-}
-else
-{
-return (NULL);
-}
-}
+		if (value < curr->n) /* insert in left subtree */
+		{
+			if (curr->left != NULL)
+				return (bst_insert(&curr->left, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->left = new);
+		}
+		if (value > curr->n) /* insert in right subtree */
+		{
+			if (curr->right != NULL)
+				return (bst_insert(&curr->right, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->right = new);
+		}
+	}
+	return (NULL);
 }
